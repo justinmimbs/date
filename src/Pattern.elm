fe52f486bf7c47de4b9225692a5bb67f63facda2
Justin Mimbs <justin.mimbs@gmail.com>
2018-05-14 13:44:16 -0400

    Add Pattern.fromString

--- src/Pattern.elm
+++ src/Pattern.elm
@@ -1,34 +1,37 @@
-module Pattern exposing (..)
+module Pattern exposing (Pattern, Token(..), fromString)
 
 import Char
 import Parser exposing ((|.), (|=), Parser)
 
 
-ex : List (Result (List Parser.DeadEnd) (List Field))
-ex =
-    [ "aaa"
-    , "abbccc"
-    ]
-        |> List.map (Parser.run parsePattern)
+type alias Pattern =
+    List Token
 
 
-
--------------------------------------------------------------------------------
-
-
-type Field
+type Token
     = Field Char Int
+    | Literal String
 
 
-parseField : Parser Field
-parseField =
+fromString : String -> Pattern
+fromString str =
+    Parser.run (patternHelp []) str
+        |> Result.withDefault [ Literal str ]
+
+
+
+-- parser
+
+
+field : Parser Token
+field =
     Parser.chompIf Char.isAlpha
         |> Parser.getChompedString
-        |> Parser.andThen parseFieldRepeats
+        |> Parser.andThen fieldRepeats
 
 
-parseFieldRepeats : String -> Parser Field
-parseFieldRepeats str =
+fieldRepeats : String -> Parser Token
+fieldRepeats str =
     case String.toList str of
         [ char ] ->
             Parser.succeed (\x y -> Field char (1 + (y - x)))
@@ -37,20 +40,80 @@ parseFieldRepeats str =
                 |= Parser.getOffset
 
         _ ->
-            Parser.problem "expected one char"
+            Parser.problem "expected exactly one char"
 
 
-parsePattern : Parser (List Field)
-parsePattern =
-    parsePatternHelp []
+escapedQuote : Parser Token
+escapedQuote =
+    Parser.succeed (Literal "'")
+        |. Parser.token "''"
 
 
-parsePatternHelp : List Field -> Parser (List Field)
-parsePatternHelp fields =
-    Parser.lazy <|
-        \_ ->
-            Parser.oneOf
-                [ parseField
-                    |> Parser.andThen (\field -> parsePatternHelp (field :: fields))
-                , Parser.succeed (List.reverse fields)
-                ]
+literal : Parser Token
+literal =
+    Parser.succeed ()
+        |. Parser.chompIf isLiteralChar
+        |. Parser.chompWhile isLiteralChar
+        |> Parser.getChompedString
+        |> Parser.map Literal
+
+
+isLiteralChar : Char -> Bool
+isLiteralChar char =
+    char /= '\'' && not (Char.isAlpha char)
+
+
+quoted : Parser Token
+quoted =
+    Parser.succeed Literal
+        |. Parser.chompIf ((==) '\'')
+        |= quotedHelp ""
+        |. Parser.oneOf
+            [ Parser.chompIf ((==) '\'')
+            , Parser.end -- lenient parse for unclosed quotes
+            ]
+
+
+quotedHelp : String -> Parser String
+quotedHelp result =
+    Parser.oneOf
+        [ Parser.succeed ()
+            |. Parser.chompIf ((/=) '\'')
+            |. Parser.chompWhile ((/=) '\'')
+            |> Parser.getChompedString
+            |> Parser.andThen (\str -> quotedHelp (result ++ str))
+        , Parser.token "''"
+            |> Parser.andThen (\_ -> quotedHelp (result ++ "'"))
+        , Parser.succeed result
+        ]
+
+
+patternHelp : List Token -> Parser (List Token)
+patternHelp tokens =
+    Parser.oneOf
+        [ Parser.oneOf
+            [ field
+            , literal
+            , escapedQuote
+            , quoted
+            ]
+            |> Parser.andThen (\token -> patternHelp (token :: tokens))
+        , Parser.lazy
+            (\_ -> Parser.succeed (finalize tokens))
+        ]
+
+
+{-| Reverse list and combine consecutive Literals.
+-}
+finalize : List Token -> List Token
+finalize =
+    List.foldl
+        (\token tokens ->
+            case ( token, tokens ) of
+                ( Literal x, (Literal y) :: rest ) ->
+                    Literal (x ++ y) :: rest
+
+                _ ->
+                    token :: tokens
+        )
+        []

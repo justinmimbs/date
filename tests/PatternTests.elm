2ca5f3ecc769804795cbf1d480c6f2bd79e0e2c9
Justin Mimbs <justin.mimbs@gmail.com>
2018-05-14 14:17:35 -0400

    Add tests for Pattern.fromString

---
+++ tests/PatternTests.elm
@@ -0,0 +1,35 @@
+module PatternTests exposing (..)
+
+import Pattern exposing (Pattern, Token)
+import Shim exposing (Expectation, Test, describe, equal, test)
+
+
+f : Char -> Int -> Token
+f =
+    Pattern.Field
+
+
+l : String -> Token
+l =
+    Pattern.Literal
+
+
+test_fromString : Test
+test_fromString =
+    let
+        toTest : ( String, Pattern ) -> Test
+        toTest ( input, expected ) =
+            test input <| \() -> Pattern.fromString input |> equal expected
+    in
+    describe "fromString" <|
+        List.map
+            toTest
+            [ ( "aaa", [ f 'a' 3 ] )
+            , ( "abbccc", [ f 'a' 1, f 'b' 2, f 'c' 3 ] )
+            , ( "''dddd''eeeee", [ l "'", f 'd' 4, l "'", f 'e' 5 ] )
+            , ( "aa-bb-cc//#!0.dd", [ f 'a' 2, l "-", f 'b' 2, l "-", f 'c' 2, l "//#!0.", f 'd' 2 ] )
+            , ( "a'''bbb'", [ f 'a' 1, l "'bbb" ] )
+            , ( "a'''bbb", [ f 'a' 1, l "'bbb" ] )
+            , ( "'o''clock'", [ l "o'clock" ] )
+            , ( "'''aaa ' '' - ''' '' '' '..' a '", [ l "'aaa  ' - ' ' ' .. a " ] )
+            ]
---
+++ tests/Shim.elm
@@ -0,0 +1,67 @@
+module Shim exposing (Expectation, Test, describe, equal, run, test)
+
+-- temporary shim for elm-test
+
+
+type Test
+    = One String (() -> Expectation)
+    | Many String (List Test)
+
+
+type Expectation
+    = Pass
+    | NotEqual ( String, String )
+
+
+equal : a -> a -> Expectation
+equal x y =
+    if x == y then
+        Pass
+    else
+        NotEqual ( Debug.toString x, Debug.toString y )
+
+
+test : String -> (() -> Expectation) -> Test
+test =
+    One
+
+
+describe : String -> List Test -> Test
+describe =
+    Many
+
+
+{-| test -> ( count of tests run, [ ( failing test context, not-equal pair ) ] )
+-}
+run : Test -> ( Int, List ( List String, ( String, String ) ) )
+run t =
+    case t of
+        One desc thunk ->
+            ( 1
+            , case thunk () of
+                Pass ->
+                    []
+
+                NotEqual pair ->
+                    [ ( [ desc ], pair ) ]
+            )
+
+        Many desc tests ->
+            List.foldl
+                (\t1 result ->
+                    run t1
+                        |> Tuple.mapSecond (List.map (Tuple.mapFirst ((::) desc)))
+                        |> append result
+                )
+                empty
+                tests
+
+
+empty : ( Int, List a )
+empty =
+    ( 0, [] )
+
+
+append : ( Int, List a ) -> ( Int, List a ) -> ( Int, List a )
+append ( n2, list2 ) ( n1, list1 ) =
+    ( n1 + n2, list1 ++ list2 )
